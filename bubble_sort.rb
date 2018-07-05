38c840d63c4e04c51107629d3588ad776afca7bb
rawenfalls <zaqmond@yandex.ru>
2018-07-05 09:55:00 +0300

    v0.01

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,11 @@
 input = gets.chomp.strip.scan(/[1-9]+/)
 size = input.length - 1
-while
-size-=1
-  break if size.negative?
-  for i in 0..size
+until size.negative?
+  for i in 0..size-1
     if input[i+1] < input[i]
       input[i], input[i + 1] = input[i + 1], input[i]
     end
   end
+  size-=1
 end
 puts input.join(" ")
